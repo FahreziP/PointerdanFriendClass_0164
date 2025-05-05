ae87e504b5122b1ff166ebe16ce0102758868e40
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-05 15:33:21 +0700

    menambahkan Pointer Dereference pMhs dengan Arrow Operator

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -19,5 +19,8 @@ int main(){
     refMhs.nim = 2; //Member Access Operator
     mhs.showNim();
 
-    
+    mahasiswa *pMhs = &mhs; //Pointer Derefrence pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
 }
