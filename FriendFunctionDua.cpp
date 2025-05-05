40a64ebad5eb5bb960ca44159cb0836abed8fc38
FahreziP <fahrezipurwadianitra@gmail.com>
2025-05-05 16:18:18 +0700

    int main friend class

--- FriendFunctionDua.cpp
+++ FriendFunctionDua.cpp
@@ -15,4 +15,20 @@ class pelajar{
 
     public:
     pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
 };
+
+void manusia::showNilaiPelajar(pelajar &x){
+    cout << x.nilai;
+}
+
+void manusia::showNilaiPelajarDua(pelajar &y){ //Tambahan atau Percobaan
+    cout << y.nilai;
+}
+
+int main(){
+    Manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}
